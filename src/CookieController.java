53af85d2a81e89ae9e545ed0debba8117c30b9ed
kabilangr <g.r.kabilan@gmail.com>
2020-08-17 11:44:49 +0530

    finish

---
+++ src/CookieController.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class CookieController
+ */
+@WebServlet("/controller")
+public class CookieController extends HttpServlet {
+
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+ String name=request.getParameter("name");
+ String pass=request.getParameter("password");
+ Cookie ck=new Cookie("Username",name);
+ response.addCookie(ck);
+ 
+ HttpSession session=request.getSession();
+ session.setAttribute("Password", pass);
+ response.sendRedirect("result.jsp");
+ 
+	}
+
+}
